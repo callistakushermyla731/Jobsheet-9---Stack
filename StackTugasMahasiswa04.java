1b69f23e7cf8f221f5f12b971f44f9e185e5ab97
Callista Dinar Kushermyla <callistakushermyla731@gmail.com>
2026-04-22 10:23:58 +0700

    Percobbaan 2

--- StackTugasMahasiswa04.java
+++ StackTugasMahasiswa04.java
@@ -71,6 +71,20 @@ public class StackTugasMahasiswa04 {
     }
 
     public int count() {
-    return top + 1;
-}
+        return top + 1;
+    }
+
+    public String konversiDesimalKeBiner(int nilai) {
+        StackKonversi04 stack = new StackKonversi04();
+        while (nilai > 0) {
+            int sisa = nilai % 2;
+            stack.push(sisa);
+            nilai = nilai / 2;
+        }
+        String biner = new String();
+        while (!stack.isEmpty()) {
+            biner += stack.pop();
+        }
+        return biner;
+    }
 }
